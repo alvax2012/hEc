14f998f7a3d729703baf904770b7f6cffbcd5293
alvax2012 <39165799+alvax2012@users.noreply.github.com>
2024-05-31 10:39:31 +0800

    1
    1

--- ConsoleApplication1/ConsoleApplication1/unit1.h
+++ ConsoleApplication1/ConsoleApplication1/unit1.h
@@ -1,15 +1,7 @@
 #pragma once
 #include "const.h"
 
+int sumArr(int a[], int n, int k);
 int prfArr(int a[], int n, int p);
 int minArr(int a[], int n);
-int sumArr(int a[], int n, int k);
-<<<<<<< HEAD
-//void prArr(int a[], int n);
-
 void prArr(int(&a)[constants::N], int n);
-=======
-
-void prArr(int a[], int n);
-//void prArr(int (&a)[4], int n)
->>>>>>> d4694548f6d6be4b731cc50ad51990bba5b43d54
